139eb7a0969303f5540c4d12f86431479d222823
mdlpontual <mdlpontual@gmail.com>
2025-09-04 19:31:37 -0300

    first commit of practice

---
+++ Models/MyFirstTestClass.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _00_IntroTestProject.Models
+{
+    public class MyFirstTestClass
+    {
+        
+    }
+}
